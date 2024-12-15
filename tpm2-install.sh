791e3600ca11147eb7fe664e663fd99c53688edc
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2024-12-15 23:55:38 +0100

    Add files via upload

---
+++ tpm2-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install -y tpm2-tools
+sudo apt-get install -y tpm2-openssl
+sudo apt-get install -y tpm2-abrmd
+sudo apt-get install -y libtpm2-pkcs11-tools
+sudo apt-get install -y libtpm2-pkcs11-1
+sudo apt-get install -y tss2
+
+echo 'A TELEPÍTÉS BEFEJEZŐDÖTT, KÉREM, HOGY INDÍTSA ÚJRA A SZÁMÍTÓGÉPÉT!'
