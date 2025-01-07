bb5a86f80067f9789163afec2253594cf2bc5093
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2025-01-07 15:58:23 +0100

    Update debian-kde-sound-install.sh

--- debian-kde-sound-install.sh
+++ debian-kde-sound-install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install -y pipewire-audio
-sudo apt-get install -y pipewire-audio-client-libraries
-sudo apt-get install -y libspa-0.2-jack
-sudo apt-get install -y gstreamer1.0-pipewire
-sudo apt-get install -y alsa-tools
+sudo apt-get install -y pulseaudio*
 sudo apt-get install -y alsa-utils
+sudo apt-get install -y alsa-tools
+sudo apt-get install -y alsa-oss
 sudo apt-get install -y alsa-firmware-loaders
 sudo apt-get autoremove --purge -y
 
