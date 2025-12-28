0d65c17c3ed55736ca6b538b953ee83422ab43de
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2025-12-28 13:14:29 +0100

    Create debian-cinnamon-desktop-install.sh

---
+++ debian-cinnamon-desktop-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install -y cinnamon
+sudo apt-get install -y cinnamon-l10n
+sudo apt-get install -y nemo
+sudo apt-get install -y gnome-terminal
+sudo apt-get install -y gnome-keyring
+sudo apt-get install -y lightdm
+sudo apt-get install -y lightdm-gtk-greeter
+sudo apt-get install -y synaptic
+sudo apt-get install -y xdg-desktop-portal
+sudo apt-get install -y xdg-desktop-portal-gtk
+sudo apt-get install -y xdg-desktop-portal-xapp
+sudo apt-get install -y network-manager
+sudo apt-get install -y network-manager-gnome
+sudo apt-get install -y wpasupplicant
