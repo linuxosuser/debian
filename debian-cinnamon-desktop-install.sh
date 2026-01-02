d3e805e2b381b320758e644a00aa10ed20c3a7f6
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2026-01-02 01:52:40 +0100

    Update debian-cinnamon-desktop-install.sh

--- debian-cinnamon-desktop-install.sh
+++ debian-cinnamon-desktop-install.sh
@@ -4,11 +4,11 @@ sudo apt-get install -y cinnamon
 sudo apt-get install -y cinnamon-l10n
 sudo apt-get install -y lightdm
 sudo apt-get install -y lightdm-gtk-greeter
-sudo apt-get install -y synaptic
 sudo apt-get install -y nemo
 sudo apt-get install -y gnome-terminal
 sudo apt-get install -y gnome-keyring
 sudo apt-get install -y libpam-gnome-keyring
+sudo apt-get install -y synaptic
 sudo apt-get install -y xdg-desktop-portal
 sudo apt-get install -y xdg-desktop-portal-gtk
 sudo apt-get install -y xdg-desktop-portal-xapp
