19f22ade87979b0432c6be73fbfde8aea8d2f32a
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2026-01-02 02:10:42 +0100

    Update debian-xfce-desktop-install.sh

--- debian-xfce-desktop-install.sh
+++ debian-xfce-desktop-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+sudo apt-get install -y linux-headers-amd64
+sudo apt-get install -y intel-microcode
 sudo apt-get install -y xfce4
 sudo apt-get install -y xfce4-goodies
 sudo apt-get install -y lightdm
 sudo apt-get install -y lightdm-gtk-greeter
-sudo apt-get install -y synaptic
 sudo apt-get install -y gnome-keyring
 sudo apt-get install -y libpam-gnome-keyring
+sudo apt-get install -y synaptic
 sudo apt-get install -y network-manager
 sudo apt-get install -y network-manager-gnome
 sudo apt-get install -y wpasupplicant
