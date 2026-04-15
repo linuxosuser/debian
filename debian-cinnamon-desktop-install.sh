9627a43fb2ff65fbc348ababbe9e315816d632f8
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2026-04-15 15:48:59 +0200

    Update debian-cinnamon-desktop-install.sh

--- debian-cinnamon-desktop-install.sh
+++ debian-cinnamon-desktop-install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-apt-get install -y linux-headers-amd64
-apt-get install -y intel-microcode
-apt-get install -y cinnamon
-apt-get install -y cinnamon-l10n
-apt-get install -y lightdm
-apt-get install -y lightdm-gtk-greeter
-apt-get install -y nemo
-apt-get install -y gnome-terminal
-apt-get install -y gnome-keyring
-apt-get install -y libpam-gnome-keyring
-apt-get install -y synaptic
-apt-get install -y xdg-desktop-portal
-apt-get install -y xdg-desktop-portal-gtk
-apt-get install -y xdg-desktop-portal-xapp
-apt-get install -y network-manager
-apt-get install -y network-manager-gnome
-apt-get install -y wpasupplicant
+apt install -y linux-headers-amd64
+apt install -y intel-microcode
+apt install -y cinnamon
+apt install -y cinnamon-l10n
+apt install -y lightdm
+apt install -y lightdm-gtk-greeter
+apt install -y nemo
+apt install -y gnome-terminal
+apt install -y gnome-keyring
+apt install -y libpam-gnome-keyring
+apt install -y synaptic
+apt install -y xdg-desktop-portal
+apt install -y xdg-desktop-portal-gtk
+apt install -y xdg-desktop-portal-xapp
+apt install -y network-manager
+apt install -y network-manager-gnome
+apt install -y wpasupplicant
 
 echo 'A TELEPÍTÉS BEFEJEZŐDÖTT, KÉREM, HOGY INDÍTSA ÚJRA A SZÁMÍTÓGÉPÉT!'
