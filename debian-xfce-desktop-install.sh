ff88b6417b1de9d19df98a509057abb02f9683e8
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2026-04-25 03:38:10 +0200

    Update debian-xfce-desktop-install.sh

--- debian-xfce-desktop-install.sh
+++ debian-xfce-desktop-install.sh
@@ -4,6 +4,7 @@ apt install -y linux-headers-amd64
 apt install -y intel-microcode
 apt install -y xfce4
 apt install -y xfce4-goodies
+apt install -y xfce4-docklike-plugin
 apt install -y lightdm
 apt install -y lightdm-gtk-greeter
 apt install -y gnome-keyring
